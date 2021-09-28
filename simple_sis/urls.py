2906b9a89995fd0f3920e4cd9a2ee4123c2e55f8
Vlad's PC <vlad@kiratech.com.au>
2021-09-29 00:32:51 +1000

    Added view activity page (WIP)

--- simple_sis/urls.py
+++ simple_sis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, login, logout
+from .views import home, login, logout, view_activity
 
 urlpatterns = [
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
     path('', home, name='home'),
+    path('activities/<int:id>', view_activity, name='view-activity'),
 ]
--- simple_sis/views.py
+++ simple_sis/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, resolve_url
+from django.http import Http404
 from django.contrib.auth import (
     authenticate,
     login as django_login,
@@ -6,6 +7,7 @@ from django.contrib.auth import (
 )
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from .models import Activity, ActivityAttendee
 
 
 def login(request):
@@ -42,8 +44,32 @@ def home(request):
 
     context = {
         'page_title': 'Home',
+        'h1_title': 'School Activities',
         'user': user,
         'activities': activities,
     }
 
     return render(request, 'home.html', context)
+
+
+@login_required
+def view_activity(request, id):
+    user = request.user
+    try:
+        activity = user.school.activities.get(id=id)
+    except Activity.DoesNotExist:
+        raise Http404("Activity does not exist")
+
+    context = {
+        'page_title': f'Activity - {activity.name}',
+        'h1_title': f'{activity.name}',
+        'user': user,
+        'activity': activity,
+        # Normally this would be done with a reverse lookup
+        # but for the save of the challenge we can run a
+        # raw SQL query
+        'organisers': activity.attendees.filter(is_organiser=True),
+        'attendees': activity.attendees.filter(is_organiser=False),
+    }
+
+    return render(request, 'activity-view.html', context)
