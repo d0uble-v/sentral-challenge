82376b93df5145d40aeaf9323749328c6954ed0c
Vlad's PC <vlad@kiratech.com.au>
2021-09-28 23:06:58 +1000

    Added models to admin panel

--- simple_sis/admin.py
+++ simple_sis/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from simple_sis.models import (
+    User,
+    UserAccountType,
+    School,
+    Activity,
+    Location,
+    Venue,
+    LookupCode,
+    LookupCodeType,
+)
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserAccountTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'location',
+    )
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    pass
+
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
+class VenueAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'location',
+    )
+
+
+class LookupCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'type',
+    )
+
+
+class LookupCodeTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+# Set header
+admin.site.site_header = 'SimpleSIS Admin'
+
+# Register models
+admin.site.register(User, UserAdmin)
+admin.site.register(UserAccountType, UserAccountTypeAdmin)
+admin.site.register(School, SchoolAdmin)
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(LookupCode, LookupCodeAdmin)
+admin.site.register(LookupCodeType, LookupCodeTypeAdmin)
